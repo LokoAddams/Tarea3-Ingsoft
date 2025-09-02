ffc9135842c596b9a060e75f94d6fa61c695330e
LokoAddams <lucasalcoba74@gmail.com>
2025-09-02 06:13:55 -0400

    <feat>: <Jugador 1 anota>

--- src/TennisScore/tennisGame.js
+++ src/TennisScore/tennisGame.js
@@ -2,10 +2,15 @@ class TennisGame {
   constructor() {
     this.p1 = 0;
     this.p2 = 0;
+    this.points = ['Love', '15', '30', '40'];
+  }
+
+  player1Scores() {
+    this.p1++;
   }
 
   score() {
-    return 'Love-Love';
+    return `${this.points[this.p1]}-${this.points[this.p2]}`;
   }
 }
 
--- src/TennisScore/tennisGame.test.js
+++ src/TennisScore/tennisGame.test.js
@@ -6,3 +6,11 @@ describe('Tennis Score - Inicio del game', () => {
     expect(game.score()).toBe('Love-Love');
   });
 });
+
+describe('Tennis Score - Jugador 1 anota', () => {
+  test('cuando Jugador 1 anota una vez, el marcador es 15-Love', () => {
+    const game = new TennisGame();
+    game.player1Scores();
+    expect(game.score()).toBe('15-Love');
+  });
+});
